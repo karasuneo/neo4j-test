f7b0f29673475cc0a19d25e22158f75c13e560c4
Yusei Mizutani <yusei4520030401@gmail.com>
2022-12-01 12:28:37 +0900

    neo4jとの接続

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -37,6 +37,6 @@ func helloWorld(uri, username, password string) (string, error) {
 }
 
 func main() {
-	trunsctionResult, err := helloWorld("bolt://neo4j:57687", "neo4j", "r3uyP84Mx8eevrZ")
+	trunsctionResult, err := helloWorld("neo4j://172.18.0.1:57687", "neo4j", "admin")
 	fmt.Println(trunsctionResult, err)
 }
